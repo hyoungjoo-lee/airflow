4917852c703d0b7ce56f21581d9d291677787c24
hyoungjoo <mclleehj@gmail.com>
2024-10-14 17:32:31 +0900

    dags_python_with_postgres_hook_bulk_load 업로드

---
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+with DAG(
+    dag_id='dags_python_with_postgres_hook_bulk_load',
+    start_date=pendulum.datetime(2024, 10, 1, tz='Asia/Seoul'),
+    schedule=None,
+    catchup=False
+) as dag:
+    
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        postgres_hook = PostgresHook(postgres_conn_id)
+        postgres_hook.bulk_load(tbl_nm, file_nm)
+
+    insrt_postgres = PythonOperator(
+        task_id='inst_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id' : 'conn-db-postgres-custom',
+                   'tbl_nm' : 'TbUseDaystatusView',
+                   'file_nm' : '/opt/airflow/files/TbUseDaystatusView/20241012/TbUseDaystatusView.csv'
+                   }
+    )
+
+    insrt_postgres
